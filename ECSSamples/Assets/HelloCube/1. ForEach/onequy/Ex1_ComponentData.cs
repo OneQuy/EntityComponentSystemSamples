38e2bc92ed6b4b2481f75c4114fe5252f97039b4
Minh Quy <onequy@gmail.com>
2020-09-14 07:53:30 +0700

    codeeeee

---
+++ ECSSamples/Assets/HelloCube/1. ForEach/onequy/Ex1_ComponentData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct Ex1_ComponentData : IComponentData
+{
+    public float rotateField;
+}
